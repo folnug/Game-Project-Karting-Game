b187c3cc173a9bbeae3f89c9ec9a7b2babc942d2
DrMuikku <olli.t.kokko@gmail.com>
2022-03-25 15:18:19 +0200

    Lap count ja Timer
    Toimiva lap counter ja timer. Ei välitä missä järjestyksessä checkpointeista mennään läpi.

---
+++ KART DEMO/Assets/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime;
+    public Text currentTimeText;
+    private bool maalissa = false;
+
+    void Start()
+    {
+        //Aloitusarvo
+        currentTime = 0;
+    }
+
+    void Update()
+    {
+        if (maalissa)
+            return;
+
+        //T‰ss‰ kello k‰y koko ajan
+        currentTime = currentTime + Time.deltaTime;
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:ff");
+    }
+
+    //Kun saa viestin Goalista t‰‰ pys‰ytt‰‰ timerin
+    void GoalUpdate()
+    {
+        maalissa = true;
+        currentTimeText.color = Color.yellow;
+    }
+}
+
