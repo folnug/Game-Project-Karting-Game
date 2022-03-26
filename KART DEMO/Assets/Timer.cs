d327e595a0238f0d16fd0e5931b39e43328f03e6
DrMuikku <olli.t.kokko@gmail.com>
2022-03-26 15:42:09 +0200

    Hienompi loppu
    Total time isolla tekstillä keskellä ruutua

--- KART DEMO/Assets/Timer.cs
+++ KART DEMO/Assets/Timer.cs
@@ -8,6 +8,7 @@ public class Timer : MonoBehaviour
 {
     float currentTime;
     public Text currentTimeText;
+    public Text finalTimeText;
     private bool maalissa = false;
 
     void Start()
@@ -25,13 +26,16 @@ public class Timer : MonoBehaviour
         currentTime = currentTime + Time.deltaTime;
         TimeSpan time = TimeSpan.FromSeconds(currentTime);
         currentTimeText.text = time.ToString(@"mm\:ss\:ff");
+        finalTimeText.text = time.ToString(@"mm\:ss\:ff");
     }
 
     //Kun saa viestin Goalista t‰‰ pys‰ytt‰‰ timerin
     void GoalUpdate()
     {
         maalissa = true;
-        currentTimeText.color = Color.yellow;
+        currentTimeText.color = Color.green;
+        finalTimeText.gameObject.SetActive(maalissa);
+        finalTimeText.color = Color.green;
     }
 }
 
