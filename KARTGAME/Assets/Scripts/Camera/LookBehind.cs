f8ed02b9e433568ea79b431e1bc6bd6bfdc38984
Sam Whitley <sam.whitley@student.hamk.fi>
2022-03-30 15:35:48 +0300

    Added LookBehind and Cinemachine Collider
    -Changed Active Input Handling from "Input System Package (New)" to "Both".
    -Added LookBehind (both for controller and keyboard (JoystickButton4 (LB) and KeyCode.C)).
    -Changed folder structure in Scripts folder for Music and Camera.
    -Added Cinemachine Collider to eliminate camera clipping.
    -Updated the Controls page to include Look Behind.

---
+++ KARTGAME/Assets/Scripts/Camera/LookBehind.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Cinemachine;
+
+public class LookBehind : MonoBehaviour
+{
+    CinemachineVirtualCamera cVirCam;
+    CinemachineTransposer cT;
+
+    void Start()
+    {
+        cVirCam = GetComponent<CinemachineVirtualCamera>();
+        cT = cVirCam.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.JoystickButton4))
+            cT.m_FollowOffset.z = 10.73f;
+        
+        else
+            cT.m_FollowOffset.z = -10.73f;
+    }
+}
