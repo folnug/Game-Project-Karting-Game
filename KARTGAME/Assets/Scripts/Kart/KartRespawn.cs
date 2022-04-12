380135c3205e476615c3d34a52956256a5b44a79
Sam Whitley <sam.whitley@student.hamk.fi>
2022-04-12 17:04:55 +0300

    Updated Track01 + Added KartRespawnValue

--- KARTGAME/Assets/Scripts/Kart/KartRespawn.cs
+++ KARTGAME/Assets/Scripts/Kart/KartRespawn.cs
@@ -6,11 +6,12 @@ public class KartRespawn : MonoBehaviour
 {
     [SerializeField] Transform sphere;
     [SerializeField] Transform kart;
+    [SerializeField] float kartRespawnValue;
 
     void Update() {
         if (sphere == null && kart == null) return;
 
-        if (sphere.position.y < -5f) {
+        if (sphere.position.y < kartRespawnValue) {
             sphere.position = transform.position;
             kart.rotation = transform.rotation;
         }
